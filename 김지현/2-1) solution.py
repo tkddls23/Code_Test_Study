9c6cf8dae60ad5220644a23cbc6eed03dc4683a4
Jihyun Kim <ang21icjh@gmail.com>
2022-03-11 01:56:14 +0900

    fix 2-1: range

--- 김지현/2-1) solution.py
+++ 김지현/2-1) solution.py
@@ -1,14 +1,12 @@
-# len(L) >=2 라는 전제가 깔린 문제인지?
-
 def solution(L, x):
-    if x <= L[0]: # min(L)로도 가능
+    if x < L[0]: # min(L)로도 가능
         L.insert(0, x)
 
     elif x > L[-1]: # max(L)로도 가능
         L.append(x)
 
     else:
-        for i in range(1, len(L) - 1):
+        for i in range(len(L) - 1):
             if L[i] <= x <= L[i+1]:
                 L.insert(i+1, x)
                 break

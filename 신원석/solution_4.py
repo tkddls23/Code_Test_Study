84f750df5f1ee5d3dc9283868d4988bae5f24892
Shin won seok <tkddls23@konkuk.ac.kr>
2022-03-13 02:57:07 +0900

    solve: 4 - memoization

--- 신원석/solution_4.py
+++ 신원석/solution_4.py
@@ -1,8 +1,8 @@
-# 재귀적 방법
+# 재귀적 방법 O(2^n)
 def solution(x):
     return x if x < 2 else solution(x-1) + solution(x-2)
 
-# 반복적 방법
+# 반복적 방법 O(n)
 
 
 def solution(x):
@@ -13,3 +13,19 @@ def solution(x):
         Fs, Fe = Fe, Fs+Fe
 
     return Fs
+
+# 메모이제이션 O(n)
+
+
+def solution(x):
+
+    memo = [0 for i in range(x+1)]
+
+    if x < 2:
+        return x
+    else:
+        if memo[x] > 0:
+            return memo[x]
+        else:
+            memo[x] = solution(x-1) + solution(x-2)
+            return memo[x]

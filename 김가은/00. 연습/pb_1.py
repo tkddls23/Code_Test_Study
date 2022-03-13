6d05c7b8c59eca9fad5b5d5304798588ec4e9a95
Gaeun Kim <gaeun0204@gmail.com>
2022-03-13 11:49:05 +0900

    1강 풀이

---
+++ 김가은/00. 연습/pb_1.py
@@ -0,0 +1,13 @@
+# * 문제설명 *
+# 입력으로 주어지는 리스트 x 의 첫 원소와 마지막 원소의 합을 리턴하는 함수 
+# solution() 을 완성하세요.
+
+# * TestCase *
+# x - [5,3,8,2]
+# return - 7
+
+def solution(x):
+    return x[0] + x[-1]
+
+# x = [5,3,8,2]
+# print(solution(x))

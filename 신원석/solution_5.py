ea76f2669097ad777a124f8d7efd0b9e99449de2
Shin won seok <tkddls23@konkuk.ac.kr>
2022-03-21 19:23:48 +0900

    add: commet

--- 신원석/solution_5.py
+++ 신원석/solution_5.py
@@ -9,3 +9,11 @@ def solution(L, x, l, u):
 
     else:
         return solution(L, x, mid+1, u)
+
+# 이진탐색
+'''
+중간부터 탐색 시작 
+찾으려는 원소(x)가 mid에 위치한 값과 같으면 mid return
+mid에 위치값보다 작으면 last인덱스값(u)를 mid-1로 설정하고 다시 solution() 실행
+mid에 위치값보다 크면 first인텍스값(l)를 mid+1로 설정하고 다시 solution() 실행
+'''

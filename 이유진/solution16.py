83c800e491178c9bd051562dc8f6697c59b9c1f3
Yujin Lee <hojen5589@naver.com>
2022-04-04 19:38:07 +0900

    solve 13~16

---
+++ 이유진/solution16.py
@@ -0,0 +1,294 @@
+* solution.py *
+
+class Node:
+
+    def __init__(self, item):
+        self.data = item
+        self.prev = None
+        self.next = None
+
+
+class DoublyLinkedList:
+
+    def __init__(self):
+        self.nodeCount = 0
+        self.head = Node(None)
+        self.tail = Node(None)
+        self.head.prev = None
+        self.head.next = self.tail
+        self.tail.prev = self.head
+        self.tail.next = None
+
+
+    def __repr__(self):
+        if self.nodeCount == 0:
+            return 'LinkedList: empty'
+
+        s = ''
+        curr = self.head
+        while curr.next.next:
+            curr = curr.next
+            s += repr(curr.data)
+            if curr.next.next is not None:
+                s += ' -> '
+        return s
+
+
+    def getLength(self):
+        return self.nodeCount
+
+
+    def traverse(self):
+        result = []
+        curr = self.head
+        while curr.next.next:
+            curr = curr.next
+            result.append(curr.data)
+        return result
+
+
+    def reverse(self):
+        result = []
+        curr = self.tail
+        while curr.prev.prev:
+            curr = curr.prev
+            result.append(curr.data)
+        return result
+
+
+    def getAt(self, pos):
+        if pos < 0 or pos > self.nodeCount:
+            return None
+
+        if pos > self.nodeCount // 2:
+            i = 0
+            curr = self.tail
+            while i < self.nodeCount - pos + 1:
+                curr = curr.prev
+                i += 1
+        else:
+            i = 0
+            curr = self.head
+            while i < pos:
+                curr = curr.next
+                i += 1
+
+        return curr
+
+
+    def insertAfter(self, prev, newNode):
+        next = prev.next
+        newNode.prev = prev
+        newNode.next = next
+        prev.next = newNode
+        next.prev = newNode
+        self.nodeCount += 1
+        return True
+
+
+    def insertAt(self, pos, newNode):
+        if pos < 1 or pos > self.nodeCount + 1:
+            return False
+
+        prev = self.getAt(pos - 1)
+        return self.insertAfter(prev, newNode)
+
+
+    def popAfter(self, prev):
+        curr = prev.next
+        next = curr.next
+        prev.next = next
+        next.prev = prev
+        self.nodeCount -= 1
+        return curr.data
+
+
+    def popAt(self, pos):
+        if pos < 1 or pos > self.nodeCount:
+            return None
+
+        prev = self.getAt(pos - 1)
+        return self.popAfter(prev)
+
+
+    def concat(self, L):
+        self.tail.prev.next = L.head.next
+        L.head.next.prev = self.tail.prev
+        self.tail = L.tail
+
+        self.nodeCount += L.nodeCount
+
+
+class PriorityQueue:
+
+    def __init__(self):
+        self.queue = DoublyLinkedList()
+
+
+    def size(self):
+        return self.queue.getLength()
+
+    def isEmpty(self):
+        return self.size() == 0
+
+    def enqueue(self, x):
+        newNode = Node(x)
+        curr = self.queue.head #어디서부터 시작
+        
+        while curr.next.data != None and curr.next.data < x: #끝을 만나지 않고 우선순위를 만족할때까지
+            curr = curr.next
+        self.queue.insertAfter(curr, newNode) #어느위치에 삽입
+
+
+###재시도
+
+* solution.py *
+
+class Node:
+
+    def __init__(self, item):
+        self.data = item
+        self.prev = None
+        self.next = None
+
+
+class DoublyLinkedList:
+
+    def __init__(self):
+        self.nodeCount = 0
+        self.head = Node(None)
+        self.tail = Node(None)
+        self.head.prev = None
+        self.head.next = self.tail
+        self.tail.prev = self.head
+        self.tail.next = None
+
+
+    def __repr__(self):
+        if self.nodeCount == 0:
+            return 'LinkedList: empty'
+
+        s = ''
+        curr = self.head
+        while curr.next.next:
+            curr = curr.next
+            s += repr(curr.data)
+            if curr.next.next is not None:
+                s += ' -> '
+        return s
+
+
+    def getLength(self):
+        return self.nodeCount
+
+
+    def traverse(self):
+        result = []
+        curr = self.head
+        while curr.next.next:
+            curr = curr.next
+            result.append(curr.data)
+        return result
+
+
+    def reverse(self):
+        result = []
+        curr = self.tail
+        while curr.prev.prev:
+            curr = curr.prev
+            result.append(curr.data)
+        return result
+
+
+    def getAt(self, pos):
+        if pos < 0 or pos > self.nodeCount:
+            return None
+
+        if pos > self.nodeCount // 2:
+            i = 0
+            curr = self.tail
+            while i < self.nodeCount - pos + 1:
+                curr = curr.prev
+                i += 1
+        else:
+            i = 0
+            curr = self.head
+            while i < pos:
+                curr = curr.next
+                i += 1
+
+        return curr
+
+
+    def insertAfter(self, prev, newNode):
+        next = prev.next
+        newNode.prev = prev
+        newNode.next = next
+        prev.next = newNode
+        next.prev = newNode
+        self.nodeCount += 1
+        return True
+
+
+    def insertAt(self, pos, newNode):
+        if pos < 1 or pos > self.nodeCount + 1:
+            return False
+
+        prev = self.getAt(pos - 1)
+        return self.insertAfter(prev, newNode)
+
+
+    def popAfter(self, prev):
+        curr = prev.next
+        next = curr.next
+        prev.next = next
+        next.prev = prev
+        self.nodeCount -= 1
+        return curr.data
+
+
+    def popAt(self, pos):
+        if pos < 1 or pos > self.nodeCount:
+            return None
+
+        prev = self.getAt(pos - 1)
+        return self.popAfter(prev)
+
+
+    def concat(self, L):
+        self.tail.prev.next = L.head.next
+        L.head.next.prev = self.tail.prev
+        self.tail = L.tail
+
+        self.nodeCount += L.nodeCount
+
+
+class PriorityQueue:
+
+    def __init__(self):
+        self.queue = DoublyLinkedList()
+
+
+    def size(self):
+        return self.queue.getLength()
+
+    def isEmpty(self):
+        return self.size() == 0
+
+    def enqueue(self, x):
+        newNode = Node(x)
+        curr = self.queue.head #어디서부터 시작
+        
+        while curr.next.data != None and curr.next.data > x: #끝을 만나지 않고 우선순위를 만족할때까지
+            curr = curr.next
+        self.queue.insertAfter(curr, newNode) #어느위치에
+        
+    def dequeue(self):
+        return self.queue.popAt(self.queue.getLength())
+
+    def peek(self):
+        return self.queue.getAt(self.queue.getLength()).data
+
+
+def solution(x):
+    return 0
